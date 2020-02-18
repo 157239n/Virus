5e3ba89d188e542aef7dde8ccb211d7e94d13fd0
Quang Ho <157239q@gmail.com>
2020-02-18 19:49:45 +0000

    Small bug fix

--- code/model/Attack/Packages/Windows/Background/MonitorLocation/controller/updateEvent.php
+++ code/model/Attack/Packages/Windows/Background/MonitorLocation/controller/updateEvent.php
@@ -3,3 +3,4 @@
 /** @var \Kelvinho\Virus\Attack\Packages\Windows\Background\MonitorLocation\MonitorLocation $this */
 
 $this->updateEventFromController($this->requestData->postCheck("event"), $this->requestData->postCheck("unixTime"));
+$this->saveState();
--- code/model/Attack/Packages/Windows/Background/MonitorLocation/controller/updateSaved.php
+++ code/model/Attack/Packages/Windows/Background/MonitorLocation/controller/updateSaved.php
@@ -3,3 +3,4 @@
 /** @var \Kelvinho\Virus\Attack\Packages\Windows\Background\MonitorLocation\MonitorLocation $this */
 
 $this->setSavedEvents($this->requestData->postCheck("savedEvents"));
+$this->saveState();
